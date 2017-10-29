c68f7157a814ce6be9c203c0c07a98bc94dc579d
daiae <33129950+daiae@users.noreply.github.com>
2017-10-29 13:48:01 +0100

    Update Blatt1.rkt

--- Blatt1/Blatt1.rkt
+++ Blatt1/Blatt1.rkt
@@ -84,4 +84,44 @@
 (~a "Kurs San Francisco nach Honolulu: " (- 360 Kurs-SanFrancisco->Honolulu))
 (~a "Kurs Osterinsel nach Lima: " Kurs-Osterinsel->Lima)
 
-; Aufgabe 2.3
+; Aufgabe 2.3 
+; Liefert die Himmelrichtung der eingegebenen Gradzahl
+(define (grad->himmelsrichtung grad)
+  (cond [(= grad 0) 'N]
+        [(= grad 22.5) 'NNO]
+        [(= grad 45) 'NO]
+        [(= grad 67.5) 'ONO]
+        [(= grad 90) 'O]
+        [(= grad 112.5) 'OSO]
+        [(= grad 135) 'SO]
+        [(= grad 157.5) 'SSO]
+        [(= grad 180) 'S]
+        [(= grad 202.5) 'SSW]
+        [(= grad 225) 'SW]
+        [(= grad 247.5) 'WSW]
+        [(= grad 270) 'W]
+        [(= grad 292.5) 'WNW]
+        [(= grad 315) 'NW]
+        [(= grad 337.5) 'NNW]
+        [else "Keine Himmelsrichtung zu dieser Gradzahl. Hinweis: Gradzahl muss zwischen null und dreihundertsiebenunddreißig komma fünf liegen."]))
+
+; Liefert die passende Gradzahl zu der angegebenen Himmelsrichtung.
+(define (himmelsrichtung->grad himmelsrichtung)
+  (cond [(equal? himmelsrichtung 'N) 0]
+        [(equal? himmelsrichtung 'NNO) 22.5]
+        [(equal? himmelsrichtung 'NO) 45]
+        [(equal? himmelsrichtung 'ONO) 67.5]
+        [(equal? himmelsrichtung 'O) 90]
+        [(equal? himmelsrichtung 'OSO) 112.5]
+        [(equal? himmelsrichtung 'SO) 135]
+        [(equal? himmelsrichtung 'SSO) 157.5]
+        [(equal? himmelsrichtung 'S) 180]
+        [(equal? himmelsrichtung 'SSW) 202.5]
+        [(equal? himmelsrichtung 'SW) 225]
+        [(equal? himmelsrichtung 'WSW) 247.5]
+        [(equal? himmelsrichtung 'W) 270]
+        [(equal? himmelsrichtung 'WNW) 292.5]
+        [(equal? himmelsrichtung 'NW) 315]
+        [(equal? himmelsrichtung 'NNW) 337.5]
+        [else "Keine Gradzahl zu dieser Himmelsrichtung. Hinweis: Himmelsrichtung in Großbuchstaben angeben. Bsp: Ostnordost: 'ONO"])) 
+
